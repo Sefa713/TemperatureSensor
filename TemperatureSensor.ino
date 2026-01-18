b85061eb4fb1da1bfd00b5e0a0ecf4fbbe547fd6
Kwaku Nhyira Sefa-Badu <sefa-badu.2@wright.edu>
2026-01-18 07:22:33 -0500

    Initial commit for Temperature Sensor project

---
+++ TemperatureSensor.ino
@@ -0,0 +1,54 @@
+// C++ code
+//
+#include <LiquidCrystal.h>
+
+ LiquidCrystal mylcd (9,7,8,12,11,10);
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(A0, INPUT);
+  mylcd.begin(16,2);
+  
+}
+
+int get_temp(int volt){ // opening the temperature function
+  int conv_volt = volt - 0.5;
+  return conv_volt/0.01;
+  }// closing the temperature function
+
+void blinking_led(int current_temp){
+  while (current_temp > 40){
+    digitalWrite(13, HIGH);delay(1000);
+    int value = analogRead(A0);
+    int voltage = value*(5/1023); 
+    int current_temp = get_temp(voltage);
+  }
+
+}  
+
+void loop()
+{
+  int value = analogRead(A0);
+  int voltage = value*(5/1023); 
+  int current_temp = get_temp(voltage);
+  if (10 < current_temp < 30){
+    mylcd.print("");
+  }
+  else if (30 < current_temp < 40  ){
+    mylcd.clear();
+    mylcd.setCursor(0,0);
+    mylcd.print("It's getting");
+    mylcd.setCursor(0,1);  
+    mylcd.print("warm!");
+  }
+  else if (current_temp > 40)  {
+    mylcd.clear();
+    mylcd.setCursor(0,0);  
+    mylcd.print("Dangerous Heat");
+    mylcd.setCursor(0,1);  
+    mylcd.print("Levels!");
+    digitalWrite(6, HIGH);
+    blinking_led(current_temp);
+  }
+}
