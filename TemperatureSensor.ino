80e44269ff3c601bf03cacea1420681ac9fc2e73
Kwaku Nhyira Sefa-Badu <sefa-badu.2@wright.edu>
2026-01-19 11:10:38 -0500

    Completed the TemperatureSensor project with LCD display and alert system.

--- TemperatureSensor.ino
+++ TemperatureSensor.ino
@@ -12,43 +12,50 @@ void setup()
   
 }
 
-int get_temp(int volt){ // opening the temperature function
-  int conv_volt = volt - 0.5;
+float get_temp(float volt){ // opening the temperature function
+  float conv_volt = volt - 0.5;
   return conv_volt/0.01;
   }// closing the temperature function
 
-void blinking_led(int current_temp){
-  while (current_temp > 40){
+void blinking_led(float current_temp){
+  if (current_temp > 40){
     digitalWrite(13, HIGH);delay(1000);
-    int value = analogRead(A0);
-    int voltage = value*(5/1023); 
-    int current_temp = get_temp(voltage);
+    digitalWrite(13, LOW);delay(1000);
   }
 
 }  
+void vibrating_motor(float current_temp){
+  if (current_temp > 40){
+    digitalWrite(6, HIGH);delay(1000);
+    digitalWrite(6, LOW);delay(1000);
+  }
 
+}
 void loop()
 {
-  int value = analogRead(A0);
-  int voltage = value*(5/1023); 
-  int current_temp = get_temp(voltage);
-  if (10 < current_temp < 30){
-    mylcd.print("");
+  float value = analogRead(A0);
+  float voltage = value*(5.0/1023.0); 
+  float current_temp = get_temp(voltage);
+  if (10 < current_temp && current_temp <= 40){
+    mylc
+    mylcd.print("Safe Temp");
+    delay(2000);
   }
-  else if (30 < current_temp < 40  ){
+  else if (40 < current_temp && current_temp <= 70 ){
     mylcd.clear();
     mylcd.setCursor(0,0);
     mylcd.print("It's getting");
     mylcd.setCursor(0,1);  
     mylcd.print("warm!");
   }
-  else if (current_temp > 40)  {
+  else if (current_temp > 70)  {
     mylcd.clear();
     mylcd.setCursor(0,0);  
     mylcd.print("Dangerous Heat");
     mylcd.setCursor(0,1);  
     mylcd.print("Levels!");
-    digitalWrite(6, HIGH);
+    vibrating_motor(current_temp);
     blinking_led(current_temp);
+    delay(200); 
   }
 }
